b53e2fd89e888acc4511ddb61e37aeed96ed0e33
rasikarakhewar3010 <rasikarakhewar3010@gmail.com>
2025-08-25 15:54:08 +0530

    removed sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
-      <Sidebar />
+      {/* <Sidebar /> */}
       <About />
       <MissionVision />
       <NewsEvents />
